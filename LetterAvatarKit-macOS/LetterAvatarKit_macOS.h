cce583b5ccea9d742da8406832828e4a39f983ba
Matt <matt.wxw.adelaide@gmail.com>
2020-05-09 17:06:56 +0930

    add macOS target

---
+++ LetterAvatarKit-macOS/LetterAvatarKit_macOS.h
@@ -0,0 +1,19 @@
+//
+//  LetterAvatarKit_macOS.h
+//  LetterAvatarKit-macOS
+//
+//  Created by Xiaowei WANG on 9/5/20.
+//  Copyright © 2020 Victor Peschenkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LetterAvatarKit_macOS.
+FOUNDATION_EXPORT double LetterAvatarKit_macOSVersionNumber;
+
+//! Project version string for LetterAvatarKit_macOS.
+FOUNDATION_EXPORT const unsigned char LetterAvatarKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LetterAvatarKit_macOS/PublicHeader.h>
+
+
